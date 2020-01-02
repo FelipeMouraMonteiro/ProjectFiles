2f68ad2854987f4f0ddf3464f0103db76765793b
Felipe <felipe.monteiro@soulit.com.br>
2020-01-01 21:16:11 -0300

    Creation alteration of the all files

---
+++ ProjectFile/ProjectFile/AllFiles.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ProjectFile
+{
+    class AllFiles
+    {
+
+
+        #region Method
+
+        public bool DeleteFiles(string directory)
+        {
+
+            if (Directory.Exists(directory))
+            {
+                string[] files = Directory.GetFiles(directory);
+
+                if (files.Length > 0)
+                {
+                    foreach (string item in files)
+                    {
+                        var file = new FileInfo(item);
+                        file.Delete();
+                    }
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+            else
+                return false;
+
+        }
+
+
+        public bool DeleteDirectory(string directory)
+        {
+
+            if (Directory.Exists(directory))
+            {
+                DeleteFiles(directory);
+                Directory.Delete(directory);
+
+                return true;
+            }
+
+            else
+                return false;
+        }
+
+        public bool CopyFiles(string origin, string destiny)
+        {
+            if (Directory.Exists(origin))
+            {
+                string[] files = Directory.GetFiles(origin);
+
+                if (Directory.Exists(destiny))
+                {
+                    DeleteDirectory(destiny);
+                    Directory.CreateDirectory(destiny);
+                }
+                else
+                    Directory.CreateDirectory(destiny);
+
+                foreach (string fil in files)
+                {
+                    var item = new FileInfo(fil);
+                    Console.WriteLine(" - " + item.Name);
+                    item.CopyTo(Path.Combine(destiny, item.Name));
+                }
+
+                if (Directory.Exists(destiny))
+                {
+                    string[] dest = Directory.GetFiles(destiny);
+
+                    if (dest.Length > 0)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    return false;
+
+            }
+            else
+                return false;
+
+        }
+        #endregion
+
+
+
+
+    }
+}
--- ProjectFile/ProjectFile/Program.cs
+++ ProjectFile/ProjectFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ProjectFile
 {
@@ -6,7 +7,35 @@ namespace ProjectFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            try
+            {
+                AllFiles file = new AllFiles();
+
+
+                string origin = @"C:\Users\Administrador\Desktop\TESTE";
+
+                string destiny = @"C:\Users\Administrador\Desktop\SAIDA";
+
+
+                if (file.CopyFiles(origin,destiny))
+                {
+                    
+                    Console.WriteLine("Cópia efetuada com sucesso!");
+   
+                }
+                else
+                    Console.WriteLine("Cópia não efetuada!");
+
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error Files ");
+                Console.WriteLine(e.Message);
+
+            }
         }
     }
 }
